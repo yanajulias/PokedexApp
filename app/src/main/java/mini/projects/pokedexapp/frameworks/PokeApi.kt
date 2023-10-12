b6edc81b7ace511d42cfa8e9fb98c61f3bf67d8e
Yana <44026935+yanajulias@users.noreply.github.com>
2023-10-12 07:56:54 +0700

    Create 'PokeApi' service

---
+++ app/src/main/java/mini/projects/pokedexapp/frameworks/PokeApi.kt
@@ -0,0 +1,21 @@
+package mini.projects.pokedexapp.frameworks
+
+import mini.projects.pokedexapp.frameworks.model.PokemonDetail
+import mini.projects.pokedexapp.frameworks.model.PokemonList
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PokeApi {
+
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("limit") limit:Int,
+        @Query("offset") offset:Int,
+    ): PokemonList
+
+    @GET("pokemon/{name}")
+    suspend fun getPokemonDetail(
+        @Path("name") name: String,
+    ): PokemonDetail
+}
